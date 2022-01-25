2dd5656cefd9c1a4230f2b991f971e35079acb96
Chheng Afa <afachheng@gmail.com>
2022-01-25 16:10:58 +0700

    Dinosaur can run but it flash flash with birds,when press space can see a dinosaur in the sky

---
+++ DinosaurGame/assets/classes/Bird.js
@@ -0,0 +1,42 @@
+class Bird {
+    shape = null;
+    size = null;
+    width = 20;
+    height = 30;
+    positionX = null;
+    positionY = null;
+    speed = 30;
+    frameIndex = 0;
+  
+    constructor(gameContext) {
+      this.gameContext = gameContext;
+      this.shape = new Image();
+      this.shape.src = "assets/img/bird.png";
+      this.positionX = this.gameContext.canvas.width;
+      this.positionY = 200;
+  
+      this.shape.onload = (() => {
+        console.log('draw image');
+        this.gameContext.ctx.drawImage(this.shape, 0, 0, 200, 200, this.gameContext.canvas.width, this.gameContext.canvas.height, 75, 75);
+      })
+    }
+    move = (() => {
+      if (this.frameIndex > 9) {
+        this.frameIndex = 0;
+      }
+      setTimeout(() => {
+        if (this.positionX > -200) {
+          this.positionX = this.positionX - this.speed;
+          this.gameContext.ctx.clearRect(0, 0, this.gameContext.canvas.width, this.gameContext.canvas.height); 
+          console.log(this.positionX);
+          this.gameContext.ctx.drawImage(
+            this.shape, 
+            this.frameIndex * 200, 0, 200, 200, // Source image
+            this.positionX, this.positionY, 75, 75 // Destination
+          );
+          requestAnimationFrame(this.move)
+          this.frameIndex++;  // the same as: this.frameIndex = this.frameIndex + 1;
+        }
+      }, 100);
+    })
+  }
---
+++ DinosaurGame/assets/classes/Game.js
@@ -0,0 +1,48 @@
+class Game {
+    // Properties
+    playerName = null;
+    dinosaur = null;
+    score = null;
+    speed = 10;
+    level = 1;
+    constructor() {
+      this.canvas = document.getElementById("canvas");
+      this.ctx = this.canvas.getContext("2d");
+      this.canvas.width = window.innerWidth;
+      this.canvas.height = window.innerHeight;
+    }
+    
+    // Methods
+    start() {
+      // Receive player name and save it.
+      let playerName = document.getElementById("player_name").value;
+      if (playerName) {
+        this.playerName = playerName;
+        console.log('Game is starting!!', this);
+      } else {
+        alert('Player name is required!!');
+      }
+  
+      // Remove starting container from canvas
+      let startingContainer = document.getElementById("starting-container");
+      startingContainer.style.display = "none";
+  
+      // Initinalize dinosaur object and display it on the screen
+      let dinosaur = new Dinosaur(this);
+      dinosaur.move();
+      document.body.onkeyup = function(e) {
+        if( e.keyCode == 32 ) {
+          dinosaur.jump();
+        }
+
+      }
+      console.log(dinosaur.move());
+  
+      // Initialize tree object and display it on the screen
+      let bird = new Bird(this);
+      bird.move();
+    }
+    pause() {
+      console.log('Pause the game');
+    }
+  }
